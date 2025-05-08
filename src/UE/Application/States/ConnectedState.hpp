4110c5ee7fee3e26dd84ad23213029501de220f7
SwierczekPaulina <paulina.swierczek2003@gmail.com>
2025-05-08 17:12:29 +0200

    Continue sms-handling

--- src/UE/Application/States/ConnectedState.hpp
+++ src/UE/Application/States/ConnectedState.hpp
@@ -14,19 +14,22 @@ public:
     // IBtsEventsHandler interface
     void handleDisconnected() override;
     void handleSms(common::PhoneNumber from, const std::string& text) override;
-    
+
     // IUserEventsHandler interface
     void handleHomeClicked() override;
     void handleSmsComposeClicked() override;
     void handleSmsViewClicked() override;
-    
+
 private:
     void showMenuView();
     void showSmsListView();
     void showSmsComposerView();
+    void showSmsView(const SmsMessage& message);
+    void handleSmsSend();
     void refreshMessageIndicator();
-    
+
     SmsDb smsDb;
+    std::vector<SmsMessage> currentMessagesList;
 };
 
 }
