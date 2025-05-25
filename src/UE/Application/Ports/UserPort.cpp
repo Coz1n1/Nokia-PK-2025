18c3262155b0da977ff89a0db5e6fffe1a6ab3d1
Coz1n1 <kacpertokajj@gmail.com>
2025-05-25 13:29:01 +0200

    feature(): change icon on connected

--- src/UE/Application/Ports/UserPort.cpp
+++ src/UE/Application/Ports/UserPort.cpp
@@ -38,6 +38,7 @@ void UserPort::showConnecting()
 
 void UserPort::showConnected()
 {
+    gui.showConnected();
     IUeGui::IListViewMode& menu = gui.setListViewMode();
     menu.clearSelectionList();
     menu.addSelectionListItem("Compose SMS", "");
